0294de9a8e080c7cb7bc55fd09f567dc180b89ad
Santiago Santos <santiagosebsantos@gmail.com>
2024-10-23 13:15:38 -0300

    Termine la seleccion y eliminacion de cartas

--- js/index.js
+++ js/index.js
@@ -129,8 +129,8 @@ class Jugador{
 
     imprimirCartas(){
         let cadena = "";
-        this.baraja.forEach(element => {
-            cadena += `${element.Valor} de ${element.Palo}\n`
+        this.baraja.forEach((element, indice) => {
+            cadena += `${indice + 1}) ${element.Valor} de ${element.Palo}\n`
         });
         return cadena;
     }
@@ -142,11 +142,13 @@ function repartirCartas(jugador1, jugador2, Maso){
     let posiciones = posicionDeCartasSinRepetir(Maso);
     let arrayAux = [];
     let arrayAux2 = [];
+    let j = 0;
 
-    for(let i = 0; i<10; i++){
-        arrayAux[i] = Maso[posiciones[i]];
-        i+=1;                                   // 'I' se incrementa para dar esa efecto de repartir una carta a la vez para cada uno
-        arrayAux2[i] = Maso[posiciones[i]];    
+    for(let i = 0; i<5; i++){
+        arrayAux[i] = Maso[posiciones[j]];
+        j+=1;                                   // 'J' se incrementa para dar esa efecto de repartir una carta a la vez para cada uno
+        arrayAux2[i] = Maso[posiciones[j]];  
+        j+=1;  
     }
 
     jugador1.baraja= arrayAux;
@@ -180,6 +182,28 @@ function posicionDeCartasSinRepetir(maso){
 
 const seleccionDeFigura = (figuraDeCartas) => figuraDeCartas[Math.floor(Math.random() * (figuraDeCartas.length-1))];
 
+function seleccionDeCartaATirar(jugador, ronda){
+    let posCarta = 0;
+    let cartaSeleccionada;
+        do{
+            posCarta = parseInt(prompt("Jugador " + jugador.nombre + " tus cartas son:\n" + jugador.imprimirCartas() + "\n¿Cuál deseas tirar?"));
+            if(posCarta<1 || posCarta>5) alert("Ingreso un numero incorrecto.\nVuelva a ingresar un numero correcto.");
+        } while(!(posCarta>=1 && posCarta<=5))
+
+            //alert("\nLa carta que tiro fue " + jugador1.baraja[posCarta-1].Valor + " de " + jugador1.baraja[posCarta-1].Palo); 
+            cartaSeleccionada = jugador.baraja[posCarta-1]           
+            jugador.baraja.splice(posCarta-1,1);
+            alert("El jugador " + jugador.nombre + " tiro 1 " + ronda );
+            console.log(jugador.imprimirCartas());
+            return cartaSeleccionada;
+}
+
+function esMentira(){
+    
+    let respuesta = parseInt(prompt("¿El jugador miente?\n1) Si\n2) No")); 
+    return respuesta === 1 ? true : false;
+}
+
 
 function mentiroso(){
 
@@ -190,12 +214,34 @@ function mentiroso(){
     jugador2.verCartas();
 
     let ronda =seleccionDeFigura(figuraDeCartas);
+    let flag = false;
+    let cartaJ1, cartaJ2;
+    let mentiroso = false;
 
     alert("En esta ronda se podran tirar solamente figuras de: " + ronda);
     
     while(true){
-        alert("Jugador " + jugador1.nombre + " tus cartas son\n" + jugador1.imprimirCartas())
-        console.log(jugador1.imprimirCartas())
+        //let a = parseInt(prompt("Jugador " + jugador1.nombre + " tus cartas son:\n" + jugador1.imprimirCartas() + "\n¿Cuál deseas tirar?"));
+       /* let posCarta = 0;
+        do{
+            posCarta = parseInt(prompt("Jugador " + jugador1.nombre + " tus cartas son:\n" + jugador1.imprimirCartas() + "\n¿Cuál deseas tirar?"));
+            if(posCarta<1 || posCarta>5) alert("Ingreso un numero incorrecto.\nVuelva a ingresar un numero correcto.");
+        } while(!(posCarta>=1 && posCarta<=5))
+
+            //alert("\nLa carta que tiro fue " + jugador1.baraja[posCarta-1].Valor + " de " + jugador1.baraja[posCarta-1].Palo); 
+
+            jugador1.baraja.splice(posCarta-1,1);
+            alert("El jugador " + jugador1.nombre + " tiro 1 " + ronda );
+            console.log(jugador1.imprimirCartas());
+
+        alert("Jugador " + jugador2.nombre + " tus cartas son:\n" + jugador2.imprimirCartas())*/
+        if(flag) mentiroso = esMentira();
+        //Acá cortar antes de preguntar, el juego termina 
+        cartaJ1 = seleccionDeCartaATirar(jugador1, ronda);
+        console.log(cartaJ1);
+        flag = true;        //El flag pasa a valer true ya que estaba en false para evitar su ejecucion en el primer turno.
+        cartaJ2 = seleccionDeCartaATirar(jugador2, ronda);
+        console.log(cartaJ2);
         break;
     }
 
